e18623d142ac61130408da593bbccf9245b56a6e
remyyounes <ryounes@gmail.com>
2017-10-26 10:19:35 -0700

    trying multiple endpoints

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const path = require('path')
 const { exportToDestination } = require('./migrator.js')
 const dt = require('./dependency-tree.js')
-const { concat } = require('ramda')
-const { debug } = require('./lib.js')
+const { concat, flatten } = require('ramda')
+const { debug, mapP } = require('./lib.js')
 
 const packageJson = '/Users/remyy/Applications/ruby/procore/wrench/package.json'
 const packageConfig = require(packageJson)
 const packages = Object.keys(packageConfig.dependencies)
 const rootDir = '/Users/remyy/Applications/ruby/procore/wrench/'
 
-const entryPoint = path.join(
-  rootDir,
-  'src/tools/budgetViewer/mounts/ProjectLevel/View.js'
-)
 
-const getDependencies = dt({
+const config = {
   packages,
   extensions: [
     '/index.jsx',
@@ -28,21 +24,35 @@ const getDependencies = dt({
   alternatePaths: [
     `${rootDir}/src/_shared`,
   ],
-})
+}
+const getDependencies = dt(config)
 
-const extraFiles = [
-  entryPoint,
-  path.join(rootDir, 'src', 'assets'),
+
+const entryPoint = path.join(
+  rootDir,
+  'src/tools/budgetViewer/mounts/ProjectLevel/View.js'
+)
+
+const entryPoints = [
+  path.join(rootDir, 'src/tools/budgetViewer/mounts/projectLevel/View.js'),
+  path.join(rootDir, 'src/tools/budgetViewer/mounts/projectLevel/DeleteSnapshot.js'),
+  path.join(rootDir, 'src/tools/budgetViewer/mounts/projectLevel/CreateSnapshot.js'),
+  path.join(rootDir, 'src/tools/budgetViewer/mounts/companyLevel/Edit.js'),
+  path.join(rootDir, 'src/tools/budgetViewer/mounts/companyLevel/Create.js'),
 ]
 
-
-const dependencies = getDependencies(entryPoint)
+const dependencies = Promise.all(entryPoints.map(getDependencies))
+  .then(flatten)
 
 dependencies
   .then(x => x.map(debug))
   .then(x => debug(x.length))
 
-const destinationRoot = '/Users/remyy/Applications/ruby/procore/hydra_clients/budget/'
-dependencies
-  .then(concat(extraFiles))
-  .then(exportToDestination(rootDir, destinationRoot))
+// const destinationRoot = '/Users/remyy/Applications/ruby/procore/hydra_clients/budget/'
+// const extraFiles = [
+//   entryPoint,
+//   path.join(rootDir, 'src', 'assets'),
+// ]
+// dependencies
+//   .then(concat(extraFiles))
+//   .then(exportToDestination(rootDir, destinationRoot))
